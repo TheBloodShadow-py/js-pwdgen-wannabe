be383c9a7dcff3e5ecc36adeb44cec38faebd790
Fabio <thebloodshadow14@gmail.com>
2023-10-03 15:22:54 +0200

    INIT - setup

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// # Password generator
+// - #### Chiedere il nome dell'utente:
+//     - Assegnarlo ad una variabile
+// - #### Chiedere il cognome dell'utente:
+//     - Assegnarlo ad una variabile
+// - #### Chiedere il colore preferito dell'utente:
+//     - Assegnarlo ad una variabile
+// - #### Genera randomicamenta 3 numeri:
+//     - Utilizzare il metodo math random
+//     - Assegnarlo ad una variabile
+// - #### Stampa i risultati sulla pagina
+//     - Concatenare i valori e stamparli
