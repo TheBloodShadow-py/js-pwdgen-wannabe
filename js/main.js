420aea5854df00294d9934eedf25c4fb2d47a862
Fabio <thebloodshadow14@gmail.com>
2023-10-03 16:57:11 +0200

    ADD - text and spaces check

--- js/main.js
+++ js/main.js
@@ -23,7 +23,19 @@ function passwordGenerator() {
   let name = prompt("Ciao, Inserisci il tuo nome:");
   let surname = prompt("Inserisci il tuo cognome:");
   let favoriteColor = prompt("Inserisci il tuo colore preferito:");
+  if (name.length === 0 || surname.length === 0 || favoriteColor.length === 0) {
+    generatedPassword.innerHTML = "Errore, non puoi inserire un valore vuoto";
+    return;
+  } else if (
+    name.match(/\d/) !== null ||
+    surname.match(/\d/) !== null ||
+    favoriteColor.match(/\d/) !== null
+  ) {
+    generatedPassword.innerHTML = "Errore, devi inserire del testo";
+    return;
+  }
   let randomNumbers = numbersGenerator(999);
-  return (generatedPassword.innerHTML =
-    name + surname + favoriteColor + randomNumbers);
+  generatedPassword.innerHTML =
+    name.trim() + surname.trim() + favoriteColor.trim() + randomNumbers;
+  return;
 }
